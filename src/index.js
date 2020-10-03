fcc8fe18e3f7f4ee859f6ae776239f3ad4c57902
Yauheni Kapachenia <kapachenia@gmail.com>
2020-10-03 23:13:30 +0300

    feat: implement function min max avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
-exports.min = function min (array) {
+exports.min = function min (array) {  
+  if ( array == null || array.length === 0 ) {
   return 0;
+  } else {
+    let resultMin = Math.min.apply(null, array);
+  return resultMin; 
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == null || array.length === 0) {
+    return 0;
+  } else {
+    let resultMax = Math.max.apply(null, array);
+  return resultMax;
+  }
 }
 
 exports.avg = function avg (array) {
+  if (array == null || array.length === 0) {
   return 0;
+  } else {
+    let resultAvg;
+  let sum = 0;
+  for (let i = 0; i< array.length; i++) {
+    sum += array[i];
+  }
+  return resultAvg = (sum / array.length);
+  }
 }
